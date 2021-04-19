28cf08cb3e8335d3b712a88a68172302fe009db3
Carlos José Pérez Rodríguez <82826548+carlprez@users.noreply.github.com>
2021-04-19 17:20:59 -0400

    Add files via upload

---
+++ clase1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace clase1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Progr2!");
+        }
+    }
+}
